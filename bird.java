f1bb5b2da10dffd313060322a7246235d6a7a169
hieupham20701 <19519511.hieu@student.iuh.edu.vn>
2022-02-15 10:59:21 +0700

    hieu commit

---
+++ bird.java
@@ -0,0 +1,15 @@
+public class Dog {
+	private String name;
+	private int age;
+
+	public Dog() {}
+
+	public Dog(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public void bark() {
+		System.out.print("Gau Gau");
+	}
+}
